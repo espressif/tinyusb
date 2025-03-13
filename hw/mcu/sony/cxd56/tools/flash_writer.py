9ea7e4e943310b9ae7f7946ada04adc3220b4000
Moritz Schwörer <118070019+MoritzSchwoerer@users.noreply.github.com>
2025-03-13 14:46:14 +0100

    Update flash_writer.py
    Removed shell=True to prevent command injection by using a list for arguments.

--- hw/mcu/sony/cxd56/tools/flash_writer.py
+++ hw/mcu/sony/cxd56/tools/flash_writer.py
@@ -507,7 +507,7 @@ def main():
 
 	do_wait_reset = True
 	if ConfigArgs.AUTO_RESET:
-		if subprocess.call("cd " + sys.path[0] + "; ./reset_board.sh", shell=True) == 0:
+		if subprocess.call(["cd", sys.path[0], "&&", "./reset_board.sh"]) == 0:
 			print("auto reset board success!!")
 			do_wait_reset = False
 			bootrom_msg = writer.cancel_autoboot()
